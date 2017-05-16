a94040fde952ef1dbc518f1a24f854e5f0c66740
mtlazaro <maritelag@gmail.com>
2017-05-16 13:13:34 +0200

    added more joints in jointangleGUI

--- setjointangle/setjointangleGUI.py
+++ setjointangle/setjointangleGUI.py
@@ -1,9 +1,22 @@
 from Tkinter import *
 
-jointsNames = ["HeadYaw", "HeadPitch"]
+jointsNames = ["HeadYaw", "HeadPitch",
+               "LShoulderPitch", "LShoulderRoll", "LElbowYaw", "LElbowRoll", "LWristYaw",
+               "RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll", "RWristYaw"]
+
 #joint limits taken from http://doc.aldebaran.com/2-5/family/pepper_technical/joints_pep.html
 jointLimits ={'HeadYaw': (-2.0857, 2.0857),
-              'HeadPitch': (-0.7068, 0.6371)}
+              'HeadPitch': (-0.7068, 0.6371),
+              'LShoulderPitch': (-2.0857, 2.0857),
+              'LShoulderRoll': (0.0087, 1.5620),
+              'LElbowYaw': (-2.0857, 2.0857),
+              'LElbowRoll': (-1.5620, -0.0087),
+              'LWristYaw': (-1.8239, 1.8239),
+              'RShoulderPitch': (-2.0857, 2.0857),
+              'RShoulderRoll': (-1.5620, -0.0087),
+              'RElbowYaw': (-2.0857, 2.0857),
+              'RElbowRoll': (0.0087,1.5620),
+              'RWristYaw': (-1.8239, 1.8239)}
 
 class JointAnglesGUI:
 
