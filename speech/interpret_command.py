22e2f4a8c84d0eb7b70ea07ef71f5a5a19ac7766
andrea_vanzo <andrea.vanzo1@gmail.com>
2017-05-19 16:23:08 +0100

    speech package created

---
+++ speech/interpret_command.py
@@ -0,0 +1,32 @@
+import qi
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pip", type=str, default="127.0.0.1",
+                        help="Robot IP address.  On robot or Local Naoqi: use '127.0.0.1'.")
+    parser.add_argument("--pport", type=int, default=9559,
+                        help="Naoqi port number")
+    parser.add_argument("--lip", type=str, default="127.0.0.1",
+                        help="LU4R IP address.")
+    parser.add_argument("--lport", type=int, default="9001",
+                        help="LU4R listening port.")
+    args = parser.parse_args()
+    pip = args.pip
+    pport = args.pport
+    state = args.state
+
+    #Start working session
+    session = qi.Session()
+    try:
+        session.connect("tcp://" + pip + ":" + str(pport))
+    except RuntimeError:
+        print ("Can't connect to Naoqi at ip \"" + pip + "\" on port " + str(pport) +".\n"
+               "Please check your script arguments. Run with -h option for help.")
+        sys.exit(1)
+
+
+    tts_service = session.service("ALTextToSpeech")
+
+if __name__ == "__main__":
+    main()
