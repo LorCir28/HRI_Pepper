c124df326d3f731d49a4b7309c67de71026a38e1
nao <iocchi@diag.uniroma1.it>
2017-11-30 17:23:12 +0100

    pepper_cmd

--- cmd_server/pepper_cmd.py
+++ cmd_server/pepper_cmd.py
@@ -3,21 +3,22 @@
 import time
 import os
 import socket
-
+import math
 
 session = None
 tts_service = None
 memory_service = None
-
+motion_service = None
 
 # Begin/end
 
 def begin():
-	global tts_service, memory_service
+	global tts_service, memory_service, motion_service
 	print 'begin'
 
 	#Starting services
 	memory_service  = session.service("ALMemory")
+	motion_service  = session.service("ALMotion")
 	tts_service = session.service("ALTextToSpeech")
 	tts_service.setLanguage("Italian")
 
@@ -35,7 +36,8 @@ def stop():
 
 
 def forward(r=1):
-	print 'forward',r
+    global motion_service
+    print 'forward',r
     #Move in its X direction
     x = r * 0.5
     y = 0.0
@@ -43,28 +45,30 @@ def forward(r=1):
     motion_service.moveTo(x, y, theta) #blocking function
 
 def backward(r=1):
-	print 'backward',r
+    global motion_service
+    print 'backward',r
     x = -r * 0.5
     y = 0.0
     theta = 0.0
     motion_service.moveTo(x, y, theta) #blocking function
 
 def left(r=1):
-	print 'left',r
+    global motion_service
+    print 'left',r
     #Turn 90deg to the left
     x = 0.0
     y = 0.0
-    theta = math.pi/2
+    theta = math.pi/2 * r
+    print 'motion_service = ',motion_service
     motion_service.moveTo(x, y, theta) #blocking function
-	
-
 
 def right(r=1):
-	print 'right',r
+    global motion_service
+    print 'right',r
     #Turn 90deg to the right
     x = 0.0
     y = 0.0
-    theta = -math.pi/2
+    theta = -math.pi/2 * r
     motion_service.moveTo(x, y, theta) #blocking function
 	
 
