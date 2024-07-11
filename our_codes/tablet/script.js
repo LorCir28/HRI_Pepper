36cd840e94286ca0a92892d7f161af1927e92161
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2024-07-11 15:33:41 +0200

    Updated tablet

--- our_codes/tablet/script.js
+++ our_codes/tablet/script.js
@@ -9,3 +9,24 @@ function submitName() {
     const name = document.getElementById('user-name').value;
     window.location.href = `story.html?name=${name}`;
 }
+
+function startStory() {
+    window.location.href = 'story.html';
+}
+
+function enterCave() {
+    window.location.href = 'cave.html';
+}
+
+function goToVillage() {
+    window.location.href = 'village.html';
+}
+
+function solveRiddle() {
+    window.location.href = 'riddle.html';
+}
+
+function exploreFurther() {
+    window.location.href = 'explore.html';
+}
+
