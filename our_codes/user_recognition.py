7a817f93d87918a68facd0e33ee6c428dd286a57
Lorenzo Cirillo <blazkblazk@gmail.com>
2024-07-11 18:30:05 +0200

    pepper hello with its arm

--- our_codes/user_recognition.py
+++ our_codes/user_recognition.py
@@ -9,10 +9,38 @@ import os
 user = 'Claudio'
 # user = 'Lorenzo'
 
+
+def wave_hello(motion_service):
+    # Enable the arms control by the pose manager.
+    motion_service.setStiffnesses("LArm", 1.0)
+    
+    # Move the left arm to wave hello
+    names = ["LShoulderPitch", "LShoulderRoll", "LElbowYaw", "LElbowRoll", "LWristYaw"]
+    angles = [-0.7, 0.3, -1.0, -0.5, 0.3]  # Adjusted angles to raise the arm higher
+    times = [1.0, 1.0, 1.0, 1.0, 1.0]  # Time in seconds to reach the position
+    
+    # Move the arm up
+    motion_service.angleInterpolation(names, angles, times, True)
+    
+    # Wave the hand
+    for _ in range(3):
+        motion_service.setAngles("LElbowYaw", -1.0, 0.2)
+        time.sleep(0.5)
+        motion_service.setAngles("LElbowYaw", -0.8, 0.2)
+        time.sleep(0.5)
+    
+    # Return the arm to a neutral position
+    angles = [1.5, 0.3, -1.3, -1.0, 0.0]  # Neutral position angles
+    motion_service.angleInterpolation(names, angles, times, True)
+    
+    # Disable the arms control by the pose manager.
+    motion_service.setStiffnesses("LArm", 0.0)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--pip", type=str, default='127.0.0.1',
-                        help="Robot IP address.  On robot or Local Naoqi: use '127.0.0.1'.")
+                        help="Robot IP address. On robot or Local Naoqi: use '127.0.0.1'.")
     parser.add_argument("--pport", type=int, default=55776,
                         help="Naoqi port number (default: 9559)")
     parser.add_argument("--sentence", type=str, default="hello Claudio",
@@ -28,7 +56,7 @@ def main():
     language = args.language
     speed = 10
 
-    #Starting application
+    # Starting application
     try:
         connection_url = "tcp://" + pip + ":" + str(pport)
         app = qi.Application(["Say", "--qi-url=" + connection_url ])
@@ -41,35 +69,33 @@ def main():
     session = app.session
 
     tts_service = session.service("ALTextToSpeech")
-    memory_service  = session.service("ALMemory")
+    memory_service = session.service("ALMemory")
+    motion_service = session.service("ALMotion")
 
     tts_service.setLanguage(language)
     tts_service.setVolume(1.0)
     tts_service.setParameter("speed", speed)
 
-    # known_users = [] # array to save users
-
     try:
         val = memory_service.getData(user)
         strsay = 'Hello' + ' ' + user + ', I can recognize you!!'
     except:
-        memory_service.insertData(user,user)
+        memory_service.insertData(user, user)
         strsay = 'Hello' + ' ' + user + ', nice to meet you for the first time!!'
         
-    # if memory_service.getData(user):
-    #     strsay = 'Hello' + ' ' + user + ', I can recognize you!!'
-    # else:
-    #     known_users.append(user)
-    #     strsay = 'Hello' + ' ' + user + ', nice to meet you for the first time!!'
-
     tts_service.say(strsay)
     tts_service.say(strsay)
-    tts_service.say(strsay)
+    wave_hello(motion_service)
+    print("  -- Say: " + strsay)
 
-    print"  -- Say: "+strsay
-
-
-
+    # Keep the application running
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Interrupted by user, stopping application")
+        app.stop()
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
